3921d7b19bf20f37cb8826f04606afa7e8d32eca
Maia <38637390+MaiadeOlive@users.noreply.github.com>
2019-03-02 16:49:12 -0300

    Rename D005 ANTECESSOR SUCESSOR.py to D005 antecessor sucessor.py

--- D005 ANTECESSOR SUCESSOR.py
+++
@@ -1,4 +0,0 @@
-n1 = int(input('\033[36mDigite um número: \033[m'))
-a = n1 - int(1)
-b = n1 + int(1)
-print('\033[32mO número escolhido é {}\nSeu antecessor é {}\nSeu sucessor é {}\033[m'.format(n1, a, b))
